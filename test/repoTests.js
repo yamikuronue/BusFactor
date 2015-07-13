5dbe807075d559fb35af8b4fae966504a7b40f04
Yami <yamikuronue@gmail.com>
2015-07-13 17:23:16 -0400

    Fixing halfassed test

--- test/repoTests.js
+++ test/repoTests.js
@@ -6,9 +6,9 @@ describe("git adapter", function() {
 	it("should work", function(done) {
 		 this.timeout(50000);
 		gitModule.init("https://github.com/yamikuronue/BusFactor.git", function() {
-			gitModule.getOwner("tests/repoTests.js", function(err, author) {
-				assert.equal("yamikuronue", author);
-				assert.falsey(err);
+			gitModule.getOwner("test/repoTests.js", function(err, author) {
+				assert.equal("Yami", author);
+				assert.notOk(err);
 				done();
 			})
 		});
