62923c67301d42ff6309ca52785dd324a0411014
Yami <yamikuronue@gmail.com>
2015-07-15 17:36:30 -0400

    Svn works.

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var Path = require("path");
 var async = require("async");
  
 var files = [];
-var location = Path.join("tmp","wc4");
+var location = Path.join("tmp","wc1");
 
 repoAdapter.init(url, location, function(err) {
 	if (err) {
 		console.log(err); return;
 	}
-	recursive(location, [Path.join("**",".git","*"),".gitignore"], function (err, files) {
+	recursive(location, [Path.join("**",".git","*"),".gitignore",'LICENSE','README.md'], function (err, files) {
 		async.each(files,function(file, cb) {
 			file = file.replace(location + Path.sep,"");
 			console.log(file);
--- test/repoTests.js
+++ test/repoTests.js
@@ -4,12 +4,16 @@ var assert = require('chai').assert;
 var sinon = require('sinon');
 
 describe("git adapter", function() {
-	it("should work", function(done) {
+	it("should interface with git correctly", function(done) {
 		 this.timeout(50000);
-		gitModule.init("https://github.com/yamikuronue/BusFactor.git", "tmp/repo1",function() {
+		gitModule.init("https://github.com/yamikuronue/BusFactor.git", "tmp/repo1",function(err) {
+			if (err) {
+				console.log(err);
+				assert.ok(false);
+			}
 			gitModule.getOwner("test/repoTests.js", function(err, author) {
-				assert.equal("Yami", author);
 				assert.notOk(err);
+				assert.equal("Yami", author);
 				done();
 			})
 		});
@@ -17,12 +21,12 @@ describe("git adapter", function() {
 });
 
 describe("svn adapter", function() {
-	it("should work", function(done) {
-		 this.timeout(50000);
-		svnModule.init("https://github.com/yamikuronue/BusFactor/trunk", "tmp/repo2",function() {
-			svnModule.getOwner("test/repoTests.js", function(err, author) {
+	it("should interface with SVN correctly", function(done) {
+		 this.timeout(100000); //SVN is slow, it's a big repo
+		svnModule.init("https://core.svn.wordpress.org/trunk/", "tmp/repo2",function() {
+			svnModule.getOwner("license.txt", function(err, author) {
 				assert.notOk(err);
-				assert.equal("Yami", author);
+				assert.equal("ryan", author);
 				done();
 			})
 		});
