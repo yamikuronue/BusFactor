b5123500cc4c76cdd766b34519f9f22e29346d09
Yamikuronue <yamikuronue@gmail.com>
2015-07-15 12:48:19 -0400

    Basic smoke test, not that it works yet

--- test/repoTests.js
+++ test/repoTests.js
@@ -1,4 +1,5 @@
 var gitModule = require('../lib/git.js');
+var svnModule = require('../lib/svn.js');
 var assert = require('chai').assert;
 var sinon = require('sinon');
 
@@ -13,6 +14,17 @@ describe("git adapter", function() {
 			})
 		});
 	});
-	
-	
+});
+
+describe("svn adapter", function() {
+	it("should work", function(done) {
+		 this.timeout(50000);
+		svnModule.init("https://github.com/yamikuronue/BusFactor", "tmp/repo2",function() {
+			svnModule.getOwner("test/repoTests.js", function(err, author) {
+				assert.equal("Yami", author);
+				assert.notOk(err);
+				done();
+			})
+		});
+	});
 })
