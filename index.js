bed5dc607fc9debbc5367c7ec7bb2d668d10473b
Yamikuronue <yamikuronue@gmail.com>
2015-07-16 07:37:55 -0400

    SVN tweaks

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-var url = "https://github.com/SockDrawer/SockBot.git"; //TODO: paramaterize
-
-var repoAdapter = require("./lib/git"); //TODO: allow svn
 var recursive = require('recursive-readdir');
 var Path = require("path");
-
 var async = require("async");
  
 var files = [];
-var location = Path.join("tmp","wc4");
+var location = Path.join("tmp","wc4"); //TODO: this ought to be better
 
-repoAdapter.init(url, location, function(err) {
+var argv = require('yargs')
+    .option('r', {
+        alias : 'repo',
+        demand: true,
+        describe: 'Repository to scan',
+        type: 'string',
+        nargs: 1
+    })
+    .option('t', {
+        alias : 'type',
+        demand: false,
+        default: 'auto',
+        describe: 'Type of repository. Accepts "git", "svn", or "auto"',
+        type: 'string',
+        nargs: 1
+    })
+    .argv;
+
+if (argv.type === 'auto') {
+	if (argv.repo.indexOf('git') > -1) {
+		argv.type = 'git';
+		console.log("Auto-detected Git repository");
+	} else if (argv.repo.indexOf('svn') > -1) {
+		argv.type = 'svn';
+		console.log("Auto-detected SVN repository");
+	} else {
+		console.log("Could not detect repository type, please be explicit!");
+		return;
+	}
+}
+
+var repoAdapter = require("./lib/" + argv.type.toLowerCase());
+
+
+repoAdapter.init(argv.repo, location, function(err) {
 	if (err) {
 		console.log(err); return;
 	}
-	recursive(location, [Path.join("**",".git","*"),".gitignore"], function (err, lsFiles) {
+	recursive(location, [Path.join("**",".git","*"),".gitignore",Path.join("**",".svn","*")], function (err, lsFiles) {
 		async.each(lsFiles,function(file, cb) {
 			file = file.replace(location + Path.sep,"");
 			repoAdapter.getOwner(file,function(err, owner) {
@@ -26,6 +56,11 @@ repoAdapter.init(url, location, function(err) {
 				cb(err);
 			})
 		}, function(err) {
+			if (err) {
+				console.log("Error while scanning repository!");
+				console.log(err);
+				return;
+			}
 			var numfiles = files.length;
 			var candidates = {};
 			console.log("File owners:");
